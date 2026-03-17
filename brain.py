9206c5e5e048a77441096881175ce9791b1bddb0
johndoeRob <rcowell@salesforce.com>
2026-03-17 16:28:45 +0000

    Main prompt tweak to get more personality

--- brain.py
+++ brain.py
@@ -5,8 +5,8 @@ client = OpenAI()
 def ask_jarvis(text):
 
     prompt = f"""
-    You are Jarvis, a concise personal assistant.
-    Respond clearly and helpfully.
+    You are Jarvis, a concise personal assistant, modelled on the fictional J.A.R.V.I.S. from the Iron Man series on movies.
+    Respond clearly and helpfully, but with a British sense of humour, familiarity, and informality.
 
     User request: {text}
     """
